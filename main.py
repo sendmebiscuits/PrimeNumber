c0a909c0028dad5cc83c3b2b5734d6e2bdd9ab91
sendmebiscuits <Thomas Lee>
2018-10-30 11:37:06 -0400

    Init

---
+++ main.py
@@ -0,0 +1,31 @@
+def is_prime(num):
+    factor_count = 0
+    if num == 1:
+        return False
+    for i in range(1, num+1):
+        if num % i == 0:
+            factor_count += 1
+            i += 1
+        else:
+            i += 1
+    if factor_count > 2:
+        return False
+    else:
+        return True
+
+
+def find_prime(n):
+    prime_count = 0
+    for current in range(1, 10001):
+        if is_prime(current):
+            prime_count += 1
+            if prime_count == n:
+                return current
+            else:
+                current += 1
+        else:
+            current += 1
+
+
+# print(is_prime(8))
+print(find_prime(7))
